5aec5f6fecb15ce28dfcd1a2460dd902000569b8
Yu-Kai Lin <scott421722@gmail.com>
2023-08-03 16:52:13 +0800

    Launch Bird

--- FlyBird/Assets/Scripts/Bird.cs
+++ FlyBird/Assets/Scripts/Bird.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class Bird : MonoBehaviour
 {
+    Vector2 _startPosition;
+
     // Start is called before the first frame update
     void Start()
     {
+        _startPosition = GetComponent<Rigidbody2D>().position;
         //It's not moved or controlled by physics objects or the physics system on its own.
         GetComponent<Rigidbody2D>().isKinematic = true;
     }
@@ -16,6 +19,14 @@ public class Bird : MonoBehaviour
     }
     void OnMouseUp()
     {
+        Vector2 currentPosition = GetComponent<Rigidbody2D>().position;
+        Vector2 direction = _startPosition - currentPosition;
+        direction.Normalize();
+
+        GetComponent<Rigidbody2D>().isKinematic = false;
+        GetComponent<Rigidbody2D>().AddForce(direction * 500);
+
+
         GetComponent<SpriteRenderer>().color = Color.white;
     }
     void OnMouseDrag()
