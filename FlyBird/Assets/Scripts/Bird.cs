b8a386cfe58b14491efd7d48c49ffc99a2bb0d90
Yu-Kai Lin <scott421722@gmail.com>
2023-08-03 16:17:28 +0800

    Drag Bird

--- FlyBird/Assets/Scripts/Bird.cs
+++ FlyBird/Assets/Scripts/Bird.cs
@@ -7,9 +7,22 @@ public class Bird : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //It's not moved or controlled by physics objects or the physics system on its own.
+        GetComponent<Rigidbody2D>().isKinematic = true;
     }
-
+    void OnMouseDown()
+    {
+        GetComponent<SpriteRenderer>().color = Color.red;
+    }
+    void OnMouseUp()
+    {
+        GetComponent<SpriteRenderer>().color = Color.white;
+    }
+    void OnMouseDrag()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(mousePosition.x, mousePosition.y, transform.position.z);
+    }
     // Update is called once per frame
     void Update()
     {
